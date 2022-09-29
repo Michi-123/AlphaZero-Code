d167ff6d402eb6a4c6d8bda372fe2ba51b620f6c
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2022-09-29 15:40:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
 setup(
     name='alphazerocode', 
     packages=find_packages(exclude=('*.pyc',)),
-    version='0.1.2',
+    version='0.1.3',
     license='MIT', 
     install_requires=[],
     author='M.Uchiyama',
