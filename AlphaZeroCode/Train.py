ed884d4c6d77dbd61fcd815f788d15f684d12182
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2022-09-29 15:39:45 +0900

    Update Train.py
    bug fix

--- AlphaZeroCode/Train.py
+++ AlphaZeroCode/Train.py
@@ -48,7 +48,7 @@ class Train():
             # iteration_size = math.ceil(len(dataset) / self.CFG.batch_size)
 
             for i in range(0, len(dataset), self.CFG.batch_size):
-                input_features, pi, z = util.make_batch(dataset[i:i + self.CFG.batch_size])
+                input_features, pi, z = self.util.make_batch(dataset[i:i + self.CFG.batch_size])
                 self.train(input_features, pi, z)
 
             self.util.output_train_log(epoch, self.running_loss_policy, self.running_loss_value, batch_iteration_size)
