b4dbf783620e02f7e79711c6224d19f563e928f5
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2022-12-24 22:31:04 +0900

    Upadate major version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
 setup(
     name='alphazerocode', 
     packages=find_packages(exclude=('*.pyc',)),
-    version='0.2.4',
+    version='1.0.0',
     license='MIT', 
     install_requires=[],
     author='M.Uchiyama',
