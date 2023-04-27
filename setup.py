8ac26194934f09258fa3803dd0e7562f78036b4d
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-04-27 15:19:07 +0900

    Update setup.py
    MCTS bug fix.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
 setup(
     name='alphazerocode', 
     packages=find_packages(exclude=('*.pyc',)),
-    version='1.9.5',
+    version='1.9.6',
     license='MIT', 
     install_requires=[],
     author='M.Uchiyama',
